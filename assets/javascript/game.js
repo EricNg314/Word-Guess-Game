0f51b2ace23c3dbad1ed0a71c8849ec3b299ba15
Okens <Ng.Eric314@gmail.com>
2018-04-25 20:27:23 -0700

    Updating with new function getWordDisplay and getNewWord. Adding a reset and winning scores.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,71 +19,63 @@ var currWordArray = [];
 var currWordRevealed = [];
 var currWordDisplay = "";
 var prevGuesses = []; //Creating empty array.
-var gameInitiated = "false";
+var gameInitiated = false;
+var goodGuess = false;
 
-var selectedWord = potentialWords[Math.floor(Math.random() * potentialWords.length)];
-console.log(selectedWord); //Display word selected by computer.
+var selectedWord = getNewWord()
 
 for (var i = 0; i < selectedWord.length; i++) {
     currWordArray.push(selectedWord.charAt(i));
-    currWordRevealed.push(" _ ");
+    currWordRevealed.push("_");
 }
 
 
-for (var j = 0; j < currWordRevealed.length; j++) {
-    currWordDisplay += " " + currWordRevealed[j] + " ";
-}
+getWordDisplay()
 
-console.log(currWordArray);
-console.log(currWordRevealed);
-console.log(currWordDisplay);
-console.log("Game initiated: " + gameInitiated);
+// console.log(currWordArray);
+// console.log(currWordRevealed);
+// console.log(currWordDisplay);
+// console.log("Game initiated: " + gameInitiated);
 
 document.onkeyup = function () {
     //Storing user input as "userGuess" and turning it lower case for comparison.
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userGuess); //Display for reference of user choice.
+    // console.log(userGuess); //Display for reference of user choice.
 
 
     if (userGuess === " ") {
         gameInitiated = true;
-        console.log("Game initiated: " + gameInitiated);
-
+        // console.log("Game initiated: " + gameInitiated);
         outputDisplay();
-
-        // //Setting information to be provided to html with id=game.
-        // var html = "<p>What food am I thinking?</p>" +
-        //     "<p>Current Word</p>" +
-        //     "<p>" + currWordDisplay + "</p>" +
-        //     "<p> _________________________ </p>" +
-        //     "<p>Wins: " + scoreWin + "</p>" +
-        //     "<p>Losses: " + scoreLoss + "</p>" +
-        //     "<p>Guesses Left: " + guessesLeft + "</p>" +
-        //     "<p>Your guesses so far: " + prevGuesses + "</p>";
-
-        // //Set id game to be html variable.
-        // document.querySelector('#game').innerHTML = html;
 
     } else if (gameInitiated === true) {
         for (var i = 0; i < currWordArray.length; i++) {
             if (userGuess === currWordArray[i]) {
                 currWordRevealed[i] = currWordArray[i];
+                goodGuess = true;
             }
         }
         console.log(currWordRevealed);
+        console.log("Good guess? " + goodGuess);
 
-        //Output to be displayed.
-        currWordDisplay = ""; //setting word display to black to avoid writing ontop of itself.
-        for (var j = 0; j < currWordRevealed.length; j++) {
-            currWordDisplay += " " + currWordRevealed[j] + " ";
+        if (goodGuess === true) {
+            goodGuess = false;
+        } else {
+            guessesLeft--;
+            prevGuesses.push(" " + userGuess.toUpperCase());
         }
-        guessesLeft--;
+
+        getWordDisplay()
+
+        if (currWordRevealed.indexOf("_") === -1) {
+            scoreWin++;
+            getNewWord();
+            guessesLeft = resetGuesses;
+            prevGuesses.length = 0;
+        }
         console.log(currWordDisplay);
-
         outputDisplay();
-
     }
-
 }
 
 
@@ -101,4 +93,18 @@ function outputDisplay() {
 
     //Set id game to be html variable.
     document.querySelector('#game').innerHTML = html;
+}
+
+function getWordDisplay() {
+    //Output to be displayed.
+    currWordDisplay = ""; //setting word display to black to avoid writing ontop of itself.
+    for (var j = 0; j < currWordRevealed.length; j++) {
+        currWordDisplay += "  " + currWordRevealed[j] + "  ";
+    }
+}
+
+function getNewWord() {
+    selectedWord = potentialWords[Math.floor(Math.random() * potentialWords.length)];
+    console.log(selectedWord);
+    return selectedWord;
 }
